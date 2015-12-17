c0353f27870d0487e28a49ed24336d9efd9840a2
Janidu Wanigasuriya <janiduw@gmail.com>
2015-12-17 13:59:26 +1100

    Added the project structure

---
+++ Pattern Swift/Pattern Swift/Pattern Swift.h
@@ -0,0 +1,19 @@
+//
+//  Pattern Swift.h
+//  Pattern Swift
+//
+//  Created by Janidu Wanigasuriya on 17/12/2015.
+//  Copyright © 2015 Janidu Wanigasuriya. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Pattern Swift.
+FOUNDATION_EXPORT double Pattern_SwiftVersionNumber;
+
+//! Project version string for Pattern Swift.
+FOUNDATION_EXPORT const unsigned char Pattern_SwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Pattern_Swift/PublicHeader.h>
+
+
